f84e05c1866910d90f41274f06b627fa4001d9e5
igor_ <igor_@192.168.102.5>
2020-09-12 16:45:19 -0400

    Interface Reserva de laboratorio

--- src/br/edu/ifmt/cba/SuperUsuario/ReservarLaboratorio/FXMLReservarLaboratorioController.java
+++ src/br/edu/ifmt/cba/SuperUsuario/ReservarLaboratorio/FXMLReservarLaboratorioController.java
@@ -7,16 +7,38 @@ package br.edu.ifmt.cba.SuperUsuario.ReservarLaboratorio;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class FXMLReservarLaboratorioController implements Initializable {
 
+    @FXML
+    private Button btn_minimizer;
 
+    @FXML
+    private Button btn_close;
     
+
+    // fecha a tela ao clicar no botão
+    @FXML
+    void stageClose(ActionEvent event) {
+        Stage stage = (Stage) btn_close.getScene().getWindow();
+        stage.close();
+
+    }
+    //minimiza a tela ao clicar no botão
+    @FXML
+    void stageMinimizar(ActionEvent event) {
+        Stage stage = (Stage) btn_minimizer.getScene().getWindow();
+        stage.setIconified(true);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
